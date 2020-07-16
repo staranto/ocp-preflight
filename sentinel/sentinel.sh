520a9c68adc9d88c7c2f666ed19c7c65c2e8dd60
Steve Taranto <staranto@gmail.com>
2020-07-16 09:45:42 -0400

    Continued WIP.

--- sentinel/sentinel.sh
+++ sentinel/sentinel.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
 plist="2379 2380 6443 9000 9999 10250 10259 30000 32767"
 for p in $plist; do
-  ncat -lk ${p} &
+  docker run --rm --detach --publish $p:80 httpd
 done
-
-tail -f /dev/null
 
